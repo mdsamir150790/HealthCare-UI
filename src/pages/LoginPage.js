f2cf836ac4d7c218483fb0be34bbb38b3eaa1e9f
DESKTOP-680MLP8\Samir <mdsamiruddin15790@gmail.com>
2019-03-06 01:48:07 +0530

    Initial commit

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import LoginForm from '../forms/LoginForm';
+import { Redirect } from 'react-router';
+
+class LoginComponent extends Component{
+
+    state = {
+        loginMessage : ''
+    }
+
+    loginUser = (formFields) => {
+        if( formFields.username === 'admin' && formFields.password === 'admin'){
+            this.setState({ loginMessage : '' })
+            this.props.onLogin(true)
+        } else {
+            this.setState({ loginMessage : '*Invalid Username or Password' })
+        }
+        
+    }
+
+    handleLogin = (formFields) => {
+        this.loginUser(formFields);
+    }
+
+    render(){
+        return(
+            <div id="content-wrapper" className="d-flex flex-column">
+                { this.props.loggedIn && <Redirect to="/dashboard" />}
+                <div id="content" className="login-content-wrapper">
+                    <div className="row">
+                        <div className="login-form dashboard-shadow text-gray">
+                            <div className="login-contents">
+                                <LoginForm onLogin={this.handleLogin}/>
+                                <div className="form-group mb-0">
+                                    <p className="red-text">&nbsp;{this.state.loginMessage}</p>
+                                </div> 
+                            </div>         
+                        </div>
+                    </div>
+                </div>
+            </div>        
+        );
+    }
+}
+
+export default LoginComponent;
